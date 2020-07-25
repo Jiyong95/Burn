fd103f088420600390bd7f3556e1a976330c9c3e
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-07-25 22:52:00 +0900

    ~2.5

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require("express");
-//require : express라는 폴더를 node_modules에서 찾음.
+import express from "express";
+
 const app = express();
 
 const PORT = 4000;
-function handleListening() {
-	console.log(`Listening on : http://localhost:${PORT}`);
-}
+
+const handleListening = () => console.log(`Listening on : http://localhost:${PORT}`);
+	// req, res는 obj임.
+const handleHome = (req, res) => res.send("Hello from home");
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+
+app.get("/", handleHome);
+// url / 주소일 때, handleHome실행
+
+app.get("/profile", handleProfile);
+// /profile일 때, handleProfile실행
 
 app.listen(4000, handleListening);
 //localhost:4000
