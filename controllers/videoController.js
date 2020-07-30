ac5173af0cd39410d95c609ca901e1be206e9f98
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-07-30 17:11:35 +0900

    2.17

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,8 @@
-export const home = (req, res) => res.render("home");
+export const home = (req, res) => res.render("home",{pageTitle:"Home"});
 //render => views폴더에서 파일명이 home이고 확장자가 pug인 파일을 보여줌.
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+//render함수는 인자로 1.템플릿, 2.템플릿에 전달할 객체를 받음.
+export const search = (req, res) => res.render("search",{pageTitle:"Search"});
+export const upload = (req, res) => res.render("upload",{pageTitle:"Upload"});
+export const videoDetail = (req, res) => res.render("videoDetail",{pageTitle:"Video Detail"});
+export const editVideo = (req, res) => res.render("editVideo",{pageTitle:"Edit Video"});
+export const deleteVideo = (req, res) => res.render("deleteVideo",{pageTitle:"Delete Video"});
