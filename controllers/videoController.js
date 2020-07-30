7c3516841084160b919731d3e3338d36beb1700e
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-07-30 17:36:16 +0900

    2.18

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,15 @@
 export const home = (req, res) => res.render("home",{pageTitle:"Home"});
 //render => views폴더에서 파일명이 home이고 확장자가 pug인 파일을 보여줌.
 //render함수는 인자로 1.템플릿, 2.템플릿에 전달할 객체를 받음.
-export const search = (req, res) => res.render("search",{pageTitle:"Search"});
+export const search = (req, res) => {
+	const { query: { term : searchingBy } } = req;
+	//const searchingBy = req.query.term;과 같음.
+
+	//검색한 정보가 req.query.term에 들어있음.
+	//console.log(req.query);
+	res.render("search", { pageTitle: "Search" , searchingBy});
+
+}
 export const upload = (req, res) => res.render("upload",{pageTitle:"Upload"});
 export const videoDetail = (req, res) => res.render("videoDetail",{pageTitle:"Video Detail"});
 export const editVideo = (req, res) => res.render("editVideo",{pageTitle:"Edit Video"});
