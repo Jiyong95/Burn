1e024c86f60a6cc9221aa51c5e8335669e73a1bd
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-08-01 15:51:39 +0900

    2.20

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -5,7 +5,11 @@ import { users, userDetail, editProfile, changePassword } from "../controllers/u
 const userRouter = express.Router();
 //export const userRouter = express.Router();
 // 변수만 export하겠다는 의미.
-userRouter.get(routes.userDetail, userDetail);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
+userRouter.get(routes.userDetail, userDetail);
+// userDetail url주소가 users/:id임
+// 그러면 editProfile이 아래에 있으면, users/edit-Profile인데 edit-Profile부분을
+// :id로 인식함. 그래서 userDetail이 실행됨.
+//  따라서 userDetail을 editProfile보다 아래에 위치시켜야함.
 export default userRouter;
