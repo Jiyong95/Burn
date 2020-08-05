1bacd86987610cdee68911895a670edfb19a722c
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-08-05 20:14:09 +0900

    3.4

--- init.js
+++ init.js
@@ -3,11 +3,13 @@ import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
 import "./models/Video";
+import "./models/Comment";
 //Model을 DB에 적용시켜줌.
 
 const PORT = process.env.PORT || 4000;
 // PORT를 불러오는데 못찾으면 4000으로 쓰겠다.
 
-const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
+const handleListening = () =>
+	console.log(`✅ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
--- models/Video.js
+++ models/Video.js
@@ -1,25 +1,32 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
-	fileUrl: {
-		type: String,
-		required: "File URL is required"
-	},
-	title: {
-		type: String,
-		required: "Title is required"
-	},
-	description: String,
-	views: {
-		type: Number,
-		default: 0
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now
-	}
+  fileUrl: {
+    type: String,
+    required: "File URL is required",
+  },
+  title: {
+    type: String,
+    required: "Title is required",
+  },
+  description: String,
+  views: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    }
+  ],
 });
 // mongoose documentation schema section 참고.
-
+//comments : 해당 Video에 대한 Comment들의 정보들을 연결 시켜주는 것.
+//ObjectId is automactically created when Video is created
 const model = mongoose.model("Video", VideoSchema);
 export default model;
