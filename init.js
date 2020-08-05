d39b63d89e06c047e7f89429ad4235ce69627e65
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-08-05 19:51:54 +0900

    3.3

--- init.js
+++ init.js
@@ -2,6 +2,8 @@ import "./db";
 import app from "./app";
 import dotenv from "dotenv";
 dotenv.config();
+import "./models/Video";
+//Model을 DB에 적용시켜줌.
 
 const PORT = process.env.PORT || 4000;
 // PORT를 불러오는데 못찾으면 4000으로 쓰겠다.
