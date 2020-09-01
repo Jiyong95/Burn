cb60ce6f482a82bd562add70e5d24d59f0e91461
Jiyong95 <49055628+Jiyong95@users.noreply.github.com>
2020-09-01 17:48:38 +0900

    9.2

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,8 +2,39 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
+let streamObject;
+let videoRecorder;
+
+const handleVideoData = (event) => {
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click();
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  console.log(videoRecorder);
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "🐱‍🐉Start recording";
+};
+
+const startRecording = () => {
+  //console.log(streamObject);
+  videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.start();
+  //videoRecorder.start(1000);
+  // recording정보는 record가 끝나야 얻을 수 있음. 그래서 1초간격으로 정보를 얻기위해 저렇게 해줄 수 있음.
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  console.log(videoRecorder);
+  recordBtn.addEventListener("click", stopRecording);
+};
+
 //mdn - getusermedia
-const startRecording = async () => {
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -12,14 +43,18 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "🐱‍🐉Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
     recordBtn.innerHTML = "🐱‍🐉Can't record";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 if (recorderContainer) {
   init();
